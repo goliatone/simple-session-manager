e23edf3a95bee12d3ac1dd28e98007987bcfbb55
goliatone <burgosemi@gmail.com>
2018-03-25 20:39:34 -0400

    update index export objects

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+'use strict';
 /**
  * Export module simple-session-manager.
  *
  * @type {Function}
  */
-module.exports = require('./lib');
+module.exports.SessionManager = require('./lib/manager');
+
+module.exports.SessionObject = require('./lib/session');
+
+module.exports.TimeoutRequest = require('./lib/timeout-request');
+
+module.exports.store = {
+    RedisStore: require('./lib/store/redis'),
+    MemoryStore: require('./lib/store/memory')
+};
+
+module.exports.init = require('./lib/init');
